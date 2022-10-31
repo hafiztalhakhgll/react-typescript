f52a86a7c23438a6f82477cf16e065b266c1bec6
Talha Khalid <talha.khalid@greenlightlabs.tech>
2022-10-31 13:11:39 +0500

    Basic Types in TS

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,11 @@
-export default function Greet() {
+type GreetProps = {
+  name: string;
+};
+
+export default function Greet(props: GreetProps) {
   return (
     <div>
-      <h1>Welcome Talha Khalid! You have 10 unread messages</h1>
+      <h1>Welcome {props.name}! You have 10 unread messages</h1>
     </div>
   );
 }
