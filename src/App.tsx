babdfdd6c43092307012234cef6855f5b5a65db7
Talha Khalid <talha.khalid@greenlightlabs.tech>
2022-11-01 13:11:41 +0500

    children props in react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Heading from "./components/Heading";
 import Status from "./components/Status";
 
 function App() {
   return (
     <div className="App">
       <Status status="loading" />
+      <Heading>This is heading</Heading>
     </div>
   );
 }
